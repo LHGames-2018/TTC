274f5d9c195f116ffac7763e57ef32412deb6121
Alex Voro <alexander.vorobev98@outlook.com>
2018-10-06 11:09:44 -0400

    goes 1 left

--- bot/bot.py
+++ bot/bot.py
@@ -13,6 +13,9 @@ class Bot:
         self.PlayerInfo = playerInfo
 
     def execute_turn(self, gameMap, visiblePlayers):
+        
+        
+        
         """
         This is where you decide what action to take.
             :param gameMap: The gamemap.
@@ -20,7 +23,7 @@ class Bot:
         """
 
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        return create_move_action(Point(1, 0))
+        return create_move_action(Point(-1, 0))
 
     def after_turn(self):
         """
