ae9a4d0f583f163452cac349ef111443c4198b8c
Patrick Richer St-Onge <patrick@wilight.co>
2018-10-06 16:00:26 -0400

    attack when player on house

--- bot/bot.py
+++ bot/bot.py
@@ -19,26 +19,24 @@ class Bot:
             :param visiblePlayers:  The list of visible players.
         """
 
-  #      print("---")
-  #      print(self.PlayerInfo.Position)
-  #      print(self.PlayerInfo.CarriedResources)
-  #      print(self.PlayerInfo.CarryingCapacity)
-  #      print("---")
-
-        if self.PlayerInfo.CarriedResources == 500:
-            dir = -1
-        else:
+        if self.PlayerInfo.CarriedResources >= 1:
             dir = 1
+        else:
+            dir = -1
+
+        for player in visiblePlayers:
+            if player.Position == player.HouseLocation and gameMap.getTileAt(Point(self.PlayerInfo.Position.x + dir, self.PlayerInfo.Position.y)) == TileContent.House:
+                return create_attack_action(Point(dir, 0))
 
         if Point(self.PlayerInfo.Position.x + dir, self.PlayerInfo.Position.y) == self.PlayerInfo.HouseLocation:
             return create_move_action(Point(dir, 0))
-        elif (gameMap.getTileAt(Point(self.PlayerInfo.Position.x + dir, self.PlayerInfo.Position.y)) == TileContent.Player):
+        elif gameMap.getTileAt(Point(self.PlayerInfo.Position.x + dir, self.PlayerInfo.Position.y)) == TileContent.Player:
             return create_attack_action(Point(dir, 0))
-        elif (gameMap.getTileAt(Point(self.PlayerInfo.Position.x + dir, self.PlayerInfo.Position.y)) == TileContent.House):
+        elif gameMap.getTileAt(Point(self.PlayerInfo.Position.x + dir, self.PlayerInfo.Position.y)) == TileContent.House:
             return create_steal_action(Point(dir, 0))
-        elif (gameMap.getTileAt(Point(self.PlayerInfo.Position.x + dir, self.PlayerInfo.Position.y)) == TileContent.Wall):
+        elif gameMap.getTileAt(Point(self.PlayerInfo.Position.x + dir, self.PlayerInfo.Position.y)) == TileContent.Wall:
             return create_attack_action(Point(dir, 0))
-        elif (gameMap.getTileAt(Point(self.PlayerInfo.Position.x + dir, self.PlayerInfo.Position.y)) == TileContent.Resource):
+        elif gameMap.getTileAt(Point(self.PlayerInfo.Position.x + dir, self.PlayerInfo.Position.y)) == TileContent.Resource:
             return create_collect_action(Point(dir, 0))
         else:
             return create_move_action(Point(dir, 0))
